cce1bf54235303193b4865a33df01c335442b744
Thais Sagaz <haystamashiiro@gmail.com>
2015-12-14 17:40:52 -0300

    Tela de Login

---
+++ app/view/login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Log Sistema</title>
+    <style>
+      body{
+        margin: 0;
+        padding: 0;
+        background-color: #EEEEEE;
+      }
+      .center{
+        text-align: center;
+        background-color: #fff;
+        margin-left: 35%;
+        margin-right: 35%;
+        padding: 2%;
+        margin-top: 15%;
+        border-radius: 8px;
+        box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.7);
+      }
+      .topo{
+        background-color: #2196F3;
+        padding: 4%;
+
+      }
+       #botao-login{
+         width: 60%;
+         height: 5%;
+         color: #FFF;
+         background-color: #2196F3;
+
+       }
+    </style>
+    <link href='https://fonts.googleapis.com/css?family=Lato:100italic' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+    <div class="topo"></div>
+    <header>
+        <div class="center">
+
+          <form class="#" action="#" method="post" name="login">
+            <h1>Login</h1>
+            <label for="usuario">Usuario</label>
+            <input type="name" name="usuario" required autofocus=""><br><br>
+            <label for="senha">Senha</label>
+            <input type="name" name="senha" required autofocus=""><br><br>
+            <button id="botao-login" type="button" name="entrar">Entrar</button>
+          </form>
+
+        </div>
+    </header>
+  </body>
+</html>
